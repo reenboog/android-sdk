56206bd71d21d834f23c0584512c8cd9c90080ae
Alex Gievsky <alex.gievsky@gmail.com>
2017-07-17 14:05:01 +0200

    Initial commit.

---
+++ app/src/main/java/com/fidel/fidel/EnterCardDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.fidel.fidel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterCardDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_card_details);
+    }
+}
