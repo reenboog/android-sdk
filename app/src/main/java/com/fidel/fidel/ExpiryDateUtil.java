cfb48ee26c9a043ed80bc83edfc49f249a45f04e
Alex Gievsky <alex.gievsky@gmail.com>
2017-07-26 17:06:52 +0200

    Added ExpiryDateUtils helpers.

--- app/src/main/java/com/fidel/fidel/ExpiryDateUtil.java
+++ app/src/main/java/com/fidel/fidel/ExpiryDateUtil.java
@@ -1,11 +1,14 @@
 package com.fidel.fidel;
 
+import android.util.Log;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -34,12 +37,70 @@ public class ExpiryDateUtil {
         return expiry;
     }
 
+    public static String monthStringFromExpiryString(String exp) {
+        Pattern datePattern = Pattern.compile("(^0[1-9]|1[0-2]) / ([0-9][0-9])");
+
+        Matcher matcher = datePattern.matcher(exp);
+        if(matcher.find()) {
+            String monthGroup = matcher.group(1);
+
+            return monthGroup;
+        }
+
+        return null;
+    }
+
+    public static String yearStringFromExpiryString(String exp) {
+        Pattern datePattern = Pattern.compile("(^0[1-9]|1[0-2]) / ([0-9][0-9])");
+
+        Matcher matcher = datePattern.matcher(exp);
+        if(matcher.find()) {
+            String yearGroup = matcher.group(2);
+
+            return yearGroup;
+        }
+
+        return null;
+    }
+
     public static int yearInTwoDigitFormat(int year) {
         if(year < 100) {
             return year;
         } else {
             return year % MILLENIUM_BASE;
         }
+    }
+
+    public static int yearInFourDigitFormat(int year) {
+        if(year < 100) {
+            return year + MILLENIUM_BASE;
+        } else {
+            return year;
+        }
+    }
+
+    public static int monthFromString(String m) {
+        try {
+            int month = Integer.parseInt(m);
+
+            return month;
+        } catch(NumberFormatException e) {
+            Log.d("ExpiryDateUtil.DEBUG", "Failed to parse month: " + m);
+        }
+
+        return 0;
+    }
+
+    public static int yearFromString(String y) {
+        try {
+            int year = Integer.parseInt(y);
+
+            return year;
+        } catch(NumberFormatException e) {
+            Log.d("ExpiryDateUtil.DEBUG", "Failed to parse year: " + y);
+        }
+
+        return 0;
     }
 
     public static String monthNumericStringForMonth(int m) {
--- app/src/main/java/com/fidel/sample/MainActivity.java
+++ app/src/main/java/com/fidel/sample/MainActivity.java
@@ -1,8 +1,13 @@
 package com.fidel.sample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.fidel.fidel.Fidel;
 import com.fidel.fidel.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
+        Fidel.programId = "690ae72c-9f9f-4c82-b6be-37285b3dd576";
+        Fidel.apiKey = "pk_test_fb56a52e-2f13-4bc1-9e1f-14f4ae7548f6";
+        Fidel.autoScan = true;
+
+
+        Button btn = (Button)findViewById(R.id.btn_show);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fidel.present(MainActivity.this);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == Fidel.FIDEL_LINK_CARD_REQUEST_CODE) {
+            if(data != null && data.hasExtra(Fidel.FIDEL_LINK_CARD_RESULT_CARD_ID)) {
+                String cardId = data.getStringExtra(Fidel.FIDEL_LINK_CARD_RESULT_CARD_ID);
+
+                Log.d("d", "CARD ID = " + cardId);
+            }
+        }
     }
 }
